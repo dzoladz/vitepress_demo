9fcb24beb73e2668fb5f22b79bfab84d215b8997
Derek Zoladz <derek@derekzoladz.com>
2023-07-11 21:39:20 -0400

    Build: 2023-07-11b

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/vitepress_demo/',
   title: "Demo Site",
   description: "A VitePress Site",
   themeConfig: {
